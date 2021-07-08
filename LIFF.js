5440c3d2e782e0a1babeec776db83319726da7ee
htokki <86817013+htokki@users.noreply.github.com>
2021-07-08 16:25:32 +0900

    Update LIFF.js

--- LIFF.js
+++ LIFF.js
@@ -22,7 +22,7 @@ window.onload = function() {
     });
 
     const useNodeJS = false;   // if you are not using a node server, set this value to false
-    const defaultLiffId = "1655093215-OyemwQp8";   // change the default LIFF value if you are not using a node server
+    const defaultLiffId = "1656168147-djvpqvZ0";   // change the default LIFF value if you are not using a node server
 
     // DO NOT CHANGE THIS
     let myLiffId = "";
